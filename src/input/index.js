a4dfef1367260730da417abffd2bd697606b608e
gregc <greg.carson@rightmove.co.uk>
2019-01-30 08:07:59 +0000

    initial commit

---
+++ src/input/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Input extends React.Component {
+    render() {
+        return (
+            <div>
+                test<input/>
+            </div>
+        )
+    }
+}
+
+export default Input;
